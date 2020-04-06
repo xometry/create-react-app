18ac12cbd6fa6f071e244f9b558f0808134ec748
Charles <criley@xometry.com>
2020-04-06 15:02:25 -0400

    3.7.5

--- packages/react-scripts/scripts/build-component.js
+++ packages/react-scripts/scripts/build-component.js
@@ -64,8 +64,6 @@ const config = configFactory('production');
 config.output.filename = 'component.js';
 config.output.library = process.env.REACT_APP_BUILD_NAME;
 config.output.libraryTarget = 'window';
-config.plugins.shift(); // removes htmlplugin
-config.plugins.shift(); // removes InterpolateHtmlPlugin
 
 // We require that you explicitly set browsers and do not fall back to
 // browserslist defaults.
